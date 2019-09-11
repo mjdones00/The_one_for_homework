8bd35ccc72bfb06bdeb85865f8179bb9b5287e5c
mjdones00 <michael_dones00@knights.ucf.edu>
2019-09-10 20:00:50 -0400

    First comment

---
+++ Practice COP3330 Chap 1/src/module-info.java
@@ -0,0 +1,16 @@
+module cookie_cake.java {
+
+
+module cookie_cake {                         
+ 
+   module cookie_cake void main (String [] args) { 
+      int wage;
+
+      wage = 20;                          
+ 
+      System.out.print("Salary is ");         
+      System.out.println(wage * 40 * 50);     
+   } 
+}
+
+}
